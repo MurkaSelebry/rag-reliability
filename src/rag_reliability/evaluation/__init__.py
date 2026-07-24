59e98a040e2562e35f8647287de4cd223f2f502e
Murad Rezvan <murad1703@yandex.ru>
2026-07-25 02:48:26 +0300

    feat(evaluation): add null calibration

--- src/rag_reliability/evaluation/__init__.py
+++ src/rag_reliability/evaluation/__init__.py
@@ -9,13 +9,17 @@ from rag_reliability.evaluation.bootstrap import (
     paired_bootstrap,
     wilson_ci,
 )
+from rag_reliability.evaluation.nullcal import NullResult, null_calibration, percentile_of
 
 __all__ = [
     "BootstrapResult",
     "McNemarResult",
+    "NullResult",
     "PairedResult",
     "bootstrap_ci",
     "exact_mcnemar",
+    "null_calibration",
     "paired_bootstrap",
+    "percentile_of",
     "wilson_ci",
 ]
